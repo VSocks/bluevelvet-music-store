b79ce245918c857049409c305c621fa81fee0c50
VSocks <marvinpsilva2003@gmail.com>
2025-12-07 01:46:18 -0300

    feito base do CRUD baseado na aula 39

---
+++ src/main/java/com/musicstore/bluevelvet/api/request/ProductDetailRequest.java
@@ -0,0 +1,8 @@
+package com.musicstore.bluevelvet.api.request;
+
+public class ProductDetailRequest {
+
+    private String name;
+
+    private String value;
+}
---
+++ src/main/java/com/musicstore/bluevelvet/api/request/ProductDimensionRequest.java
@@ -0,0 +1,14 @@
+package com.musicstore.bluevelvet.api.request;
+
+import java.math.BigDecimal;
+
+public class ProductDimensionRequest {
+
+    private Float lenght;
+
+    private Float width;
+
+    private Float height;
+
+    private Float weight;
+}
